e945756c14d33651178841074f33c5ca67a3ca23
KovacsVica004 <74656618+KovacsVica004@users.noreply.github.com>
2024-12-03 14:35:52 +0100

    nov 29 remove

--- scripts/lang.js
+++ scripts/lang.js
@@ -81,7 +81,7 @@ function translatePageContent(language) {
           eletut_15: 'Siófok, Klapka utcai állatorvosi rendelő (magánállatorvos)',
           vissza: 'Vissza',
           //hours page
-          nyitvatartas_fontos_text: ' November 29-én (pénteken) a rendelő zárva tart! ',
+          nyitvatartas_fontos_text: '  ',
           nyitvatartas_text: "A legfrissebb nyitvatartási időpontokért látogass el Facebook oldalunkra!",
           nyitvatartas_click: "Aktuális nyitvatartásunkat facebookon is megnézheted!",
           hetfo: 'Hétfő:',
@@ -139,7 +139,7 @@ function translatePageContent(language) {
           eletut_15: 'Siófok, Klapka Street Veterinary Clinic (independent veterinarian)',
           vissza: 'Back',
           //hours page
-          nyitvatartas_fontos_text: ' On November 29 (Friday), the practice will be closed! ',
+          nyitvatartas_fontos_text: '  ',
           nyitvatartas_text: "For the latest opening hours, visit our Facebook page!",
           nyitvatartas_click: "You can also view the current opening hours on Facebook!",
           hetfo: 'Monday:',
@@ -197,7 +197,7 @@ function translatePageContent(language) {
           eletut_15: 'Siófok, Tierarztpraxis Ko-Perfekt',
           vissza: 'Zurück',
           //hours page
-          nyitvatartas_fontos_text: ' Am 29. November (Freitag) bleibt die Praxis geschlossen! ',
+          nyitvatartas_fontos_text: '  ',
           nyitvatartas_text: "Die aktuellen Öffnungszeiten finden Sie auf unserer Facebook-Seite!",
           nyitvatartas_click: "Die aktuellen Öffnungszeiten auch auf Facebook!",
           hetfo: 'Montag:',
